b05ff6271b873c28e9d165fab33981ca472126a4
owner <j13thompson@student.bridgew.edu>
2023-03-02 12:18:46 -0500

    Finished Temperature.kt

--- src/main/kotlin/Temperature.kt
+++ src/main/kotlin/Temperature.kt
@@ -1,7 +1,12 @@
 class Temperature
 {
     var celsius = 0.0
-    var fahrenheit = 32.0
+    var fahrenheit
+        get() = celsius * 1.8 + 32
+        set(value)
+        {
+            celsius = (value - 32) * 5.0/9.0
+        }
 
     override fun toString() = "$celsius Celsius is equal to $fahrenheit Fahrenheit"
 }
